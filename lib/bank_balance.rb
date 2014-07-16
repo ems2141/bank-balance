e92397dfaa4cfd90e4076153cb7095977fdc403a
Ellie Schneiders <ems2141@gmail.com>
2014-07-16 15:16:57 -0600

    WIP Commit from v1

--- lib/bank_balance.rb
+++ lib/bank_balance.rb
@@ -1,13 +1,9 @@
 class BankBalance
 
-  def initialize(cc_statement)
-    @cc_statement = cc_statement
-  end
-
-  def credit_card_total
+  def credit_card_total(cc_statement)
     total = []
 
-    lines = @cc_statement.pop(@cc_statement.length-1)
+    lines = cc_statement.pop(cc_statement.length-1)
 
     lines.each do |line|
        total << line.last
@@ -16,5 +12,23 @@ class BankBalance
     array_of_nums = total.map{|x| x.gsub("$","").to_f}
     sum = array_of_nums.inject{|sum,x| sum + x }
     "$" + sum.to_s
+  end
+
+  def return_balance(statement)
+    data = statement.pop(statement.length-1)
+    total_debits = []
+    data.each do |line|
+      unless line.last == nil
+        total_debits << line[3]
+      end
+    end
+    array_of_nums = total_debits.map{|x| x.gsub("$","").to_f}
+    sum = array_of_nums.inject{|sum,x| sum + x }
+
+
+    deposit = data.select{|x| x[1] == "Deposit Bank"}
+    balance = deposit.first[2].gsub("$", "").gsub(",", "").to_f - sum
+
+    "$" + balance.to_s
   end
 end
