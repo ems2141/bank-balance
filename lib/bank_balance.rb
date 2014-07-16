f300f68c107344725b22b2acfba54c4ac736c237
Ellie Schneiders <ems2141@gmail.com>
2014-07-16 11:54:56 -0600

    user can total credit card statement

---
+++ lib/bank_balance.rb
@@ -0,0 +1,20 @@
+class BankBalance
+
+  def initialize(cc_statement)
+    @cc_statement = cc_statement
+  end
+
+  def credit_card_total
+    total = []
+
+    lines = @cc_statement.pop(@cc_statement.length-1)
+
+    lines.each do |line|
+       total << line.last
+    end
+
+    array_of_nums = total.map{|x| x.gsub("$","").to_f}
+    sum = array_of_nums.inject{|sum,x| sum + x }
+    "$" + sum.to_s
+  end
+end
